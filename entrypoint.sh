d37c164e39c86de8569a55d2fbdab2dcad2107da
Mika Mäki <agentti002x@gmail.com>
2020-07-29 01:23:02 +0300

    Add printing of report file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,6 @@ cloc \
   ${INPUT_BY_FILE_BY_LANG:+ --by-file-by-lang} \
   ${INPUT_EXCLUDE_LIST_FILE:+ --exclude-list-file="${INPUT_EXCLUDE_LIST_FILE}"} \
   ${INPUT_REPORT_FILE:+ --report-file="${INPUT_REPORT_FILE}"}
+
+${INPUT_REPORT_FILE:+ echo }
+${INPUT_REPORT_FILE:+ cat "${INPUT_REPORT_FILE}"}
