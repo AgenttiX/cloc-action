3148a04510576ac0d88523ee249bf81c3e8607e1
Mika Mäki <agentti002x@gmail.com>
2020-07-29 01:22:49 +0300

    Remove comments from between entrypoint.sh continued lines
    Shell scripts cannot have comments between continued lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 cloc \
   ${INPUT_PATH:- "${GITHUB_WORKSPACE}"} \
-  # Input Options
-  # Processing Options
   ${INPUT_BY_FILE:+ --by-file} \
   ${INPUT_BY_FILE_BY_LANG:+ --by-file-by-lang} \
-  # Filter options
   ${INPUT_EXCLUDE_LIST_FILE:+ --exclude-list-file="${INPUT_EXCLUDE_LIST_FILE}"} \
-  # Debug Options
-  # Output Options
   ${INPUT_REPORT_FILE:+ --report-file="${INPUT_REPORT_FILE}"}
